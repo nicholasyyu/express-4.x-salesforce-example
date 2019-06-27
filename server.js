46d9a31df0e37011877ba3c5f168993320f69b95
nicholasyyu <nicholas.yu24@gmail.com>
2019-06-26 17:36:40 -0700

    Added more files

---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-salesforce-oauth2').Strategy;
+
+
+passport.use(new Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'https://localhost:3000/login/salesforce/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+  
+    return cb(null, profile);
+    
+  }));
+
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+
+var app = express();
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/salesforce',
+  passport.authenticate('salesforce'));
+
+app.get('/login/salesforce/return', 
+  passport.authenticate('salesforce', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+app.listen(3000);
